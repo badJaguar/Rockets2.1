2bfac9d698457e0d47e9c010ade3d182371ca201
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2018-12-11 17:14:09 +0300

    Initial commit

---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    public float ScrollSpeed = -5f;
+
+    private Vector2 StartPos;
+    // Use this for initialization
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float NewPos = Mathf.Repeat(Time.time * ScrollSpeed, 18);
+        transform.position = StartPos + Vector2.up * NewPos;
+    }
+}
---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.UIElements;
+
+public class RocketController : MonoBehaviour
+{
+    public float force = 5f;
+    public float rotateSpeed = 1f;
+    private Rigidbody2D player;
+    public float moveSpeed = 50f;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        var jetpackActive = Input.GetMouseButton(0);
+
+        //if (jetpackActive)
+        //{
+        //    player.AddForce(new Vector2(0, force));
+        //}
+        if (Input.GetAxis("Mouse X") < 0 && jetpackActive)
+        {
+            player.AddForce(new Vector2(-moveSpeed, 0));
+            //player.transform.localPosition += Vector3.left * Time.deltaTime * moveSpeed;
+            //player.transform.position += new Vector3(Input.GetAxis("Mouse X") * Time.deltaTime * moveSpeed, 0.0f);
+            //transform.Rotate(0, 0, -(Input.GetAxis("Mouse X")) * Time.deltaTime * rotateSpeed);
+
+        }
+
+        if (Input.GetAxis("Mouse X") > 0 && jetpackActive)
+        {
+            player.AddForce(new Vector2(moveSpeed, 0));
+            //player.transform.localPosition += Vector3.right * Time.deltaTime * moveSpeed;
+
+            //player.transform.position += new Vector3(Input.GetAxis("Mouse X") * Time.deltaTime * moveSpeed, 0.0f);
+            //transform.Rotate(0, 0, -(Input.GetAxis("Mouse X")) * Time.deltaTime * rotateSpeed);
+        }
+        else if (!jetpackActive)
+        {
+            player.Sleep();
+        }
+    }
+}
